f9927461e114557c4715f95b635be309b46b482c
GrooshBene <GrooshBene@GrooshBeneui-MacBook-Air.local>
2015-08-21 15:07:09 +0900

    Dialog Making, Parsing Done

--- app/src/main/java/grooshbene/weather/MainPage.java
+++ app/src/main/java/grooshbene/weather/MainPage.java
@@ -1,28 +1,112 @@
 package grooshbene.weather;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Calendar;
 
 
 public class MainPage extends Activity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mPlanetTitles;
+    private ListView mDrawerList;
+    TextView currentTemp;
+    String[] asdf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         TextView date = (TextView) findViewById(R.id.date);
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        currentTemp = (TextView) findViewById(R.id.current_temp);
         //id로 텍스트 불러오기
         Calendar calendar = Calendar.getInstance();
         //calendar 선언
-        int month = calendar.get(Calendar.MONTH)+1;
+        int month = calendar.get(Calendar.MONTH) + 1;
         //달
         int day = calendar.get(Calendar.DAY_OF_MONTH);
-        date.setText(month+"월"+day+"일");
+        int year = calendar.get(Calendar.YEAR);
+        date.setText(year+"년"+month + "월" + day + "일");
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                Document doc = null;
+                try {
+                    doc = Jsoup.connect("http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=109").get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Elements max_current = doc.select("location data tmx");
+                Elements min_current = doc.select("location data tmn");
+                asdf = new String[max_current.size()];
+                for(int i=0;i<max_current.size();i++) {
+                    asdf[i] = max_current.get(i).text().toString();
+                }
+                System.out.println(asdf[0]);
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
+
+//        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.activity_main_page, mPlanetTitles));
+//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+//                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+//
+//            /** Called when a drawer has settled in a completely closed state. */
+//            public void onDrawerClosed(View view) {
+//                super.onDrawerClosed(view);
+//                getActionBar().setTitle(mTitle);
+//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+//            }
+//
+//            /** Called when a drawer has settled in a completely open state. */
+//            public void onDrawerOpened(View drawerView) {
+//                super.onDrawerOpened(drawerView);
+//                getActionBar().setTitle(mDrawerTitle);
+//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+//            }
+//        };
+//
+//        // Set the drawer toggle as the DrawerListener
+//        mDrawerLayout.setDrawerListener(mDrawerToggle);
+//    }
+//
+//    /* Called whenever we call invalidateOptionsMenu() */
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        // If the nav drawer is open, hide action items related to the content view
+//        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+//        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+//}
     }
 
 
@@ -57,4 +141,6 @@ public class MainPage extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+    /** Swaps fragments in the main content view */
+
 }
